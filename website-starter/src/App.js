db19dda46603b2eb927d66bd25b7d0e943e10403
Pritesh Amin <pritesha97@gmail.com>
2022-07-18 10:45:46 -0400

    Website Starter Code

---
+++ website-starter/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Product from './components/pages/Product';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' exact element={<Home/>}/>
+        <Route path='/services' element={<Services/>} />
+        <Route path='/products' element={<Product/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ website-starter/src/components/Cards.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <div className='cards'>
+        <h1>Check out these EPIC Destinations!</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__items'>
+                    <CardItem 
+                    src='./CS.jpg'
+                    text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                    label='Adventure'
+                    path='/services'
+                    />
+
+                    <CardItem 
+                    src='./CS.jpg'
+                    text='Travel through the Islands of Bali in a private cruise'
+                    label='Luxury'
+                    path='/services'
+                    />
+                </ul>
+                <ul className='cards__items'>
+                    <CardItem 
+                    src='./CS.jpg'
+                    text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                    label='Adventure'
+                    path='/services'
+                    />
+
+                    <CardItem 
+                    src='./CS.jpg'
+                    text='Travel through the Islands of Bali in a private cruise'
+                    label='Luxury'
+                    path='/services'
+                    />
+
+                    <CardItem 
+                    src='./CS.jpg'
+                    text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                    label='Adventure'
+                    path='/services'
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
